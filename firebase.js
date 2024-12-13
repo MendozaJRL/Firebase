f2db5851e19b8149878cb56bdbc4ce098620c54a
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-12-13 23:38:56 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // For authentication
+import { getDatabase } from "firebase/database"; // For Realtime Database
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUefDN8rPiCffHw8ns2OJfKml_87lo2z4",
+  authDomain: "luminaflora-eef53.firebaseapp.com",
+  databaseURL: "https://luminaflora-eef53-default-rtdb.firebaseio.com",
+  projectId: "luminaflora-eef53",
+  storageBucket: "luminaflora-eef53.firebasestorage.app",
+  messagingSenderId: "686428803989",
+  appId: "1:686428803989:web:fa55795ce43faf2b065c9c",
+  measurementId: "G-3HTPFD2GDF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firebase services
+export const auth = getAuth(app); // Export Authentication service
+export const database = getDatabase(app); // Export Realtime Database service
+export default app;
